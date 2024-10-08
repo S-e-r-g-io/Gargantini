f929c11f01d782f9c971dd1a9bbfc9eca74215aa
ChriMiele123 <127768934+ChriMiele123@users.noreply.github.com>
2024-10-08 11:52:23 +0200

    aggiunta funzione differenza

--- calcolatore/src/main/java/nome1/calcolatore/Calcolatrice.java
+++ calcolatore/src/main/java/nome1/calcolatore/Calcolatrice.java
@@ -5,13 +5,21 @@ public class Calcolatrice {
 	private int somma(int a, int b) {
 		return a+b;
 	}
+	private int differenza(int a, int b) {
+		return a-b;
+	}
+
 
 	public static void main(String[] args) {
 		Calcolatrice c = new Calcolatrice();
 		int result = c.somma(5, 7);
+		int result2 = c.differenza(5, 7);
+
 		
 		System.out.println("Faccio la somma di 5 e 7");
 		System.out.println("La somma è "+ result);
+		System.out.println("La diff è "+ result2);
+
 		// file modificato 
 	}
 }
